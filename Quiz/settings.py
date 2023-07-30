b97f4eaa92daea8e3c9b9951681df64f248deddf
codewitgabi <gabrielmicheal497@gmail.com>
2023-07-30 05:05:24 +0100

    Changed staticfiles dirs

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 # Default primary key field type
 
