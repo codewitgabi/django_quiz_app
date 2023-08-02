739d74a5cf0c1bba4e89addf238afb38c2bfc139
codewitgabi <gabrielmicheal497@gmail.com>
2023-08-02 03:21:09 +0100

    Fixed timer localStorage issue

--- quizapp/models.py
+++ quizapp/models.py
@@ -2,29 +2,40 @@ from django.db import models
 
 
 class Quiz(models.Model):
-	name = models.CharField(max_length= 50)
-	duration = models.IntegerField(default=60, help_text="Enter quiz duration in seconds.")
-	
-	def __str__(self):
-		return self.name
-		
-	class Meta:
-		verbose_name_plural = "Quizzes"
-		
-
+    name = models.CharField(max_length= 50, unique=True)
+    duration = models.IntegerField(default=60, help_text="Enter quiz duration in seconds.")
+    
+    class Meta:
+        verbose_name_plural = "Quizzes"
+    
+    
+    @classmethod
+    def quiz_to_show(cls):
+        quizzes = []
+        
+        for quiz in cls.objects.all():
+            if quiz.question_set.all().count() > 0:
+                quizzes.append(quiz)
+        
+        return Quiz.objects.filter(name__in=quizzes)
+    
+    def __str__(self):
+        return self.name
+ 
+ 
 class Question(models.Model):
-	quiz = models.ForeignKey(Quiz, on_delete= models.CASCADE)
-	text = models.TextField()
-	
-	def __str__(self):
-		return self.text
+    quiz = models.ForeignKey(Quiz, on_delete= models.CASCADE)
+    text = models.TextField()
+    	
+    def __str__(self):
+        return self.text
 		
 		
 class Answer(models.Model):
-	question = models.ForeignKey(Question, on_delete= models.CASCADE)
-	answer = models.CharField(max_length= 50)
-	is_correct = models.BooleanField(default= False)
-	
-	def __str__(self):
-		return self.answer
+    question = models.ForeignKey(Question, on_delete= models.CASCADE)
+    answer = models.CharField(max_length= 50)
+    is_correct = models.BooleanField(default= False)
+    	
+    def __str__(self):
+        return self.answer
 
